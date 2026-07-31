f07356ccb9795280877820f30e05ca9964a186ef
Shrikant <shrikantmugati35@gmail.com>
2026-07-31 08:32:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.68%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    bool palindrom( string s,int y,int z){
-        if(y>z) return true;
-        if(s[y]!=s[z]) return false;
-       else  return  palindrom(s,y+1,z-1);
-    }
+    // bool palindrom( string s,int y,int z){
+    // //     if(y>z) return true;
+    // //     if(s[y]!=s[z]) return false;
+    // //    else  return  palindrom(s,y+1,z-1);
+    // // }
     bool isPalindrome(int x) {
-        string s=to_string(x);
-       return palindrom(s,0,s.length()-1);
+    //     string s=to_string(x);
+    //    return palindrom(s,0,s.length()-1);
     
-    } 
+    // } 
+    //second method//
+    if(x<0) return false;
+    int orgin=x;
+    long long rev=0;
+    while(x>0){
+        rev=rev*10 +x%10;
+        x/=10;
+    }
+    return orgin==rev;
+    }
  
 };
