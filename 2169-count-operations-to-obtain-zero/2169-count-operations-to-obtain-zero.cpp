f2d3923055859d5a1e2de37f4d5c29e11a611236
Shrikant <shrikantmugati35@gmail.com>
2026-08-09 09:26:46 +0530

    Time: 3 ms (12.78%), Space: 7.9 MB (73.2%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -4,7 +4,7 @@ public:
         int count =0;
         
         while(num1!=0 && num2!=0){
-            if(num1<num2){
+            if(num1<=num2){
                  num2=num2-num1;
              
             }
