72186d97b1b337f9e04168490d22e1e478a3061f
Shrikant <shrikantmugati35@gmail.com>
2026-08-09 09:08:57 +0530

    Time: 0 ms (100%), Space: 31.6 MB (84.49%) - LeetHub

--- 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.cpp
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int minimumSwaps(vector<int>& nums) {
+        
         int n=nums.size();
         int count=0;
          int i=0,j=n-1;
          while(i<j){
             if(nums[i]!=0 ) i++;
+            else if(nums[j]==0) j--;
             else {
                 swap(nums[i] ,nums[j]);
                 count++;
